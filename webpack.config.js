07f83d26228b11cba6dfff11a6bedb1e6ef69f6a
wudgocks <junghc0304@naver.com>
2023-01-27 18:26:32 +0900

    create project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+// export
+module.exports = {
+  entry : './js/main.js',
+  
+  // 결과물을 반환하는 설정
+  output : {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename : 'main.js',
+    clean : true
+  },
+  
+  module: {
+    rules : [
+      {
+        test : /\.s?css$/,
+        use : [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test : /\.js$/,
+        use : [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  
+  plugins : [
+    new HtmlPlugin({
+      template : './index.html'
+    }),
+    new CopyPlugin({
+      patterns : [
+        { from : 'static' }
+      ]
+    })
+  ],
+  devServer : {
+    host : 'localhost'
+  }
+}
